d5bf4ef1ee3435a55ef7fbf3232b4c75e8b8350c
DongtaiS <dongtaishi.w@gmail.com>
2022-01-20 14:35:18 -0500

    adding calendar events

--- app/src/main/java/com/example/discjockeymanager/CalendarFragment.kt
+++ app/src/main/java/com/example/discjockeymanager/CalendarFragment.kt
@@ -1,14 +1,24 @@
 package com.example.discjockeymanager
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CalendarView
+import androidx.annotation.RequiresApi
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.findNavController
 import com.applandeo.materialcalendarview.EventDay
+import com.applandeo.materialcalendarview.utils.DateUtils
+import com.example.discjockeymanager.Objects.Event
+import com.example.discjockeymanager.databinding.CalendarEventItemBinding
 import com.example.discjockeymanager.databinding.FragmentCalendarBinding
+import org.json.JSONObject
 import java.util.*
+import kotlin.math.log
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,9 +34,11 @@ class CalendarFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
+    private val eventList = ArrayList<Event>()
+    private val calendarEvents = ArrayList<EventDay>()
     private lateinit var binding: FragmentCalendarBinding
-
+    private val dayToEvent = HashMap<Calendar, ArrayList<Event>>()
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -34,12 +46,49 @@ class CalendarFragment : Fragment() {
             param2 = it.getString(ARG_PARAM2)
         }
         binding = FragmentCalendarBinding.inflate(layoutInflater)
-        val c = Calendar.getInstance()
-        c.set(2022, 0, 20, 5, 5, 5)
-        val event = EventDay(c, R.drawable.dj_manager_logo)
-        val events = listOf(event)
 
-        binding.calendar.setEvents(events)
+        APIRequestHelper.jsonRequestWithAuth(requireContext(), RequestType.GET_EVENTS, JSONObject(), {
+            Log.i("TESTREQUEST", it.toString())
+            val eventArr = it.getJSONArray("events")
+            for (i in 0 until eventArr.length()) {
+                val event = Event.parseEvent(eventArr.getJSONObject(i))
+                eventList.add(event)
+                val c = Calendar.getInstance()
+                c.clear()
+                c.set(event.bookedDate.year, event.bookedDate.monthValue-1, event.bookedDate.dayOfMonth)
+                if (!dayToEvent.containsKey(c)) {
+                    dayToEvent[c] = ArrayList<Event>()
+                }
+                dayToEvent[c]?.add(event)
+                Log.i("TEST", c.toString())
+                val e = EventDay(c, R.drawable.background_circle)
+                calendarEvents.add(e)
+            }
+            binding.calendar.setEvents(calendarEvents)
+        })
+
+        binding.calendar.setOnDayClickListener {
+            Log.i("TEST", it.calendar.toString())
+            if (dayToEvent.containsKey(it.calendar)) {
+                Log.i("TEST", "GOOD")
+                createCalendarEventItems(dayToEvent[it.calendar]!!)
+            }
+        }
+    }
+
+    fun createCalendarEventItems(events: List<Event>) {
+        binding.scrollContainerCalendar.removeAllViews()
+        for(e in events) {
+            val b = CalendarEventItemBinding.inflate(layoutInflater, binding.scrollContainerCalendar, true)
+            b.txtCalendarEventDate.text = e.date.toString()
+            b.txtCalendarEventName.text = e.eventName
+            b.layoutCalendarEvent.setOnClickListener {
+                findNavController().navigate(
+                    R.id.action_calendarFragment_to_eventPopupFragment,
+                    bundleOf("Event" to e)
+                )
+            }
+        }
     }
 
     override fun onCreateView(
